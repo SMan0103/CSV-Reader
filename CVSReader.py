69f82c1a457eb2da53df3a55c18210de90da969d
SMan0103 <33371016+SMan0103@users.noreply.github.com>
2022-03-21 21:13:01 +0100

    Add files via upload

---
+++ CVSReader.py
@@ -0,0 +1,18 @@
+import csv
+
+from collections import Counter
+
+file = open("data.csv")
+csvreader = csv.reader(file)
+header = next(csvreader)
+print(header)
+rows = []
+for row in csvreader:
+    rows.append(row)
+print(rows)
+
+
+
+file.close()
+
+
